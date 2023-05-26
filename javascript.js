7ab98cd507b291b1af1cbba7be4d62174b29e812
Vincent <Vincentnguyen030@gmail.com>
2023-05-26 00:01:04 -0500

    make constants for each button

--- javascript.js
+++ javascript.js
@@ -13,3 +13,24 @@ function multiply(num1,num2) {
 function divide(num1,num2) {
     return num1/num2
 }
+
+const clear = document.querySelector('.clear');
+const seven = document.querySelector('.seven');
+const four = document.querySelector('.four');
+const one = document.querySelector('.one');
+const enlarge = document.querySelector('.enlarge');
+const dlt = document.querySelector('.delete');
+const eight = document.querySelector('.eight');
+const five = document.querySelector('.five');
+const two = document.querySelector('.two');
+const zero = document.querySelector('.zero');
+const percent = document.querySelector('.percent');
+const nine = document.querySelector('.nine');
+const six = document.querySelector('.six');
+const three = document.querySelector('.three');
+const decimal = document.querySelector('.decimal');
+const divide = document.querySelector('.divide');
+const multiply = document.querySelector('.multiply');
+const subtract = document.querySelector('.subtract');
+const add = document.querySelector('.add');
+const equal = document.querySelector('.equal');
